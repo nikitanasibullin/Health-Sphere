e7f1a8e1bc3bef90d0da025f1c550b166e4d1b10
nikitanasibullin <nikitanasibullin2@gmail.com>
2025-12-14 23:57:26 +0300

    Added admin's method to get schedule

--- project/app/routers/admin.py
+++ project/app/routers/admin.py
@@ -482,3 +482,27 @@ def search_doctors(
 def get_doctors(db: Session = Depends(get_db),current_admin = Depends(oauth2.get_current_admin)):
     doctors= db.query(models.Doctor).all()
     return doctors
+
+
+@router.get("/schedule/{doctor_id}",response_model=List[schemas.ScheduleResponse])
+def get_doctors_schedule(doctor_id: int,current_admin = Depends(oauth2.get_current_admin), 
+                         db: Session = Depends(get_db)):
+    doctor = db.query(models.Doctor).filter(models.Doctor.id == doctor_id).first()
+    
+    if not doctor:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Врач с ID {doctor_id} не найден"
+        )
+    
+    # Создаем базовый запрос с использованием relationship
+    query = db.query(models.Schedule)\
+        .filter(models.Schedule.doctor_id == doctor_id)
+    
+    # Сортируем результаты
+    schedules = query.order_by(
+        models.Schedule.date.asc(),
+        models.Schedule.start_time.asc()
+    ).all()
+    
+    return schedules
