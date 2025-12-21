dd590a1354d7f0f2b42b2c5f965f55d614eed503
unknown <cezaryulin@gmail.com>
2025-12-21 21:04:10 +0300

    rm https

--- src/components/http.js
+++ src/components/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'https://localhost:8000',
+  baseURL: 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json'
   }
